2c80ee54aa0e6341ae8680947b6995a573d80382
akimatter <akm2000@gmail.com>
2011-05-11 05:18:40 +0900

    $ rails new pickle_sandbox --skip-test-unit $ cd pickle_sandbox/ $ git init $ echo "/database.yml" > config/.gitignore $ cp config/database.yml config/database.yml.example $ git add .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PickleSandbox::Application.config.secret_token = 'abe942cd14e3b66432c03106da81c130f8bcb87f30db603c1f501179c561b3271370ceda9f28388d29ac3d43b6ea8df880488f645de5f4785b015059a6bdeeb4'
