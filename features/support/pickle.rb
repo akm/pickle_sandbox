c16b819e99fec2e5eb8fa7489cf15adfc815c53a
akimatter <akm2000@gmail.com>
2011-05-11 12:26:20 +0900

    .featureにモデルの属性名を書けるように、Pickleを拡張しました。

--- features/support/pickle.rb
+++ features/support/pickle.rb
@@ -27,9 +27,71 @@ require 'pickle/path/world'
 require 'pickle/email/world'
 
 
+module Pickle::I18n
+  class << self
+    def model_translations
+      @model_translations ||= {}
+    end
+
+    def model_attribute_translations
+      @model_attribute_translations ||= {}
+    end
+  end
+end
+
+Pickle::I18n.model_translations["商品"] = "product"
+Pickle::I18n.model_attribute_translations["product"] = {"名称" => "name", "価格" => "price"}
+
+Pickle::Session.module_eval do
+
+  def create_model_with_i18n_attribute_names(orig_pickle_ref, orig_fields = nil)
+    pickle_ref = Pickle::I18n.model_translations[orig_pickle_ref] || orig_pickle_ref
+    raise ArgumentError, "No model_translation found: #{orig_pickle_ref}" unless orig_pickle_ref
+
+    if attr_trans = Pickle::I18n.model_attribute_translations[pickle_ref]
+      case orig_fields
+      when String then
+        fields = orig_fields.dup
+        attr_trans.each{|k,v| fields.gsub!(k, v)}
+      when Hash then
+        fields = orig_fields.inject({}){|d, (k, v)| d[ attr_trans[k] || k ] = v; d }
+      else
+        raise "Unsupported fields: #{fields.inspect}"
+      end
+    end
+    fields ||= orig_fields
+
+    create_model_without_i18n_attribute_names(pickle_ref, fields)
+  end
+
+  alias_method :create_model_without_i18n_attribute_names, :create_model
+  alias_method :create_model, :create_model_with_i18n_attribute_names
+end
+
+
+Pickle::Parser.module_eval do
+  def match_field
+    # "(?:\\w+: #{match_value})"
+    "(?:\s*[^:]+: #{match_value})"
+  end
+
+  def capture_key_and_value_in_field
+    # "(?:(\\w+): #{capture_value})"
+    "(?:\s*([^:]+): #{capture_value})"
+  end
+
+end
+
+
 Pickle.configure do |config|
   # config.factories の中身はこんな感じ
   #   {"product"=>#<Pickle::Adapter::FactoryGirl:0x00000102b0b618 @klass=Product(id: integer, name: string, price: decimal, created_at: datetime, updated_at: datetime), @name="product">}
   # モデルの日本語名についてもfactoryを設定します
-  config.factories['商品'] = config.factories['product']
+  # config.factories['商品'] = config.factories['product']
+
+  Pickle::I18n.model_translations.each do |key, value|
+    config.factories[key] = config.factories[value]
+  end
 end
+
+
